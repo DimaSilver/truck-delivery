b80f92f8080aee92572c35d1922273ae9c432943
DimaSilver <68532590+DimaSilver@users.noreply.github.com>
2020-07-20 10:49:17 +0300

    Добавил сверстанный сайт
    Добавил сверстанный сайт включающий в себя index.html, папки css, images, js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.services-slider').slick({
+		prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="" /></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="" /></button>'
+	});
+	$('.menu-btn').on('click', function(){
+		$('.menu-list').slideToggle();
+	});
+});
